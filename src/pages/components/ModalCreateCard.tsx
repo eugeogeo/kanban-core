559fd95b35b5944ca9c1f172de8e2b4ef2d5d6f6
Geovana Figueiredo Silva <geovanaf1912@gmail.com>
2026-02-04 21:43:22 -0300

    feat: criando form para criar o card

--- src/pages/components/ModalCreateCard.tsx
+++ src/pages/components/ModalCreateCard.tsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material";
+import { Stack, TextField } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
 import Modal from "../../componentes/Modal";
 
 type ModalCreateCardProps = {
@@ -6,21 +7,48 @@ type ModalCreateCardProps = {
   onClose: () => void;
 };
 
+type Inputs = {
+  title: string;
+  // column: ColumnBoard;
+};
+
 const ModalCreateCard = (data: ModalCreateCardProps) => {
   const { isOpen, onClose } = data;
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
 
   const handleClose = () => {
     onClose();
   };
 
-  const handleSubmit = () => {
+  const onSubmit = () => {
     // Lógica para criar o card
     onClose();
   };
 
   return (
-    <Modal isOpen={isOpen} title="Criar Card" onClose={handleClose} onSubmit={handleSubmit}>
-      <Typography>Criando card</Typography>
+    <Modal isOpen={isOpen} title="Criar Card" onClose={handleClose} onSubmit={handleSubmit(onSubmit)}>
+      <Stack component="form">
+        <Controller
+          name="title"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              label="Título do card"
+              size="small"
+              error={!!errors.title}
+              helperText={errors.title?.message}
+              fullWidth
+              required
+              {...field}
+            />
+          )}
+        />
+      </Stack>
     </Modal>
   );
 };
